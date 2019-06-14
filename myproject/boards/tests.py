314b416885d3a9d9886c589580d37efe282cf427
caleb <cwolfe651@gmail.com>
2019-06-13 20:38:27 -0500

    adding static files and paths

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
-
+from  django.urls import reverse
 # Create your tests here.
+class HomeTests(TestCase):
+	def test_home_view_status_code(self):
+		url = reverse('home')
+		repsonse =self.client.get(url)
+		self.assertEquals(repsonse.status_code, 200)
