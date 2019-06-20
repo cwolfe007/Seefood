e198392c138878fbbea58ea7a665fef1333e6e10
caleb <cwolfe651@gmail.com>
2019-06-19 21:50:15 -0500

    adding missing files

---
+++ myproject/accounts/views.py
@@ -0,0 +1,8 @@
+
+# Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+def signup(request):
+    form = UserCreationForm()
+    return render(request, 'signup.html', {'form':form})
